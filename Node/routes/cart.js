8fa8e80d961d56c517964acaa529c5f1ab85a0f7
tomislaaaav <jmbotelho95@gmail.com>
2017-11-09 17:31:11 +0000

    changed the sends to jsons

--- Node/routes/cart.js
+++ Node/routes/cart.js
@@ -9,10 +9,10 @@ router.get('/:idUser', function (req, res) {
 	console.log(req.params.idUser);
 	stmt.get(req.params.idUser, (err, rows) => {
 		if (rows.idUser != undefined && rows.idUser != null) {
-			res.send(rows); // id is valid
+			res.json(rows); // id is valid
 		} else {
 			console.log(rows);
-			res.send('Error'); // id doesn't exist
+			res.json('Error'); // id doesn't exist
 		}
 	});
 });
@@ -26,18 +26,18 @@ router.get('/add/:idUser/:idProduct', function (req, res) {
 				stmt = db.prepare('UPDATE CartItem SET quantity = ? WHERE idProduct = ? AND idCart = ?');
 				stmt.get([cart.quantity + 1, cart.idProduct, cart.idCart], (err, result) => {
 					console.log(result);
-					res.send('Success adding another product of the same type');
+					res.json('Success adding another product of the same type');
 				});
 			} else {
 				stmt = db.prepare('INSERT INTO CartItem (quantity, idProduct, idCart) VALUES (?, ?, ?)');
 				stmt.get([1, req.params.idProduct, cart.idCart], (err, result) => {
 					console.log(result);
-					res.send('Success adding a different product');
+					res.json('Success adding a different product');
 				});
 			}
 		} else {
 			console.log(cart);
-			res.send('Error');
+			res.json('Error');
 		}
 	});
 });
@@ -51,15 +51,15 @@ router.get('/remove/:idUser/:idProduct', function (req, res) {
 				stmt = db.prepare('DELETE FROM CartItem WHERE idProduct = ? AND idCart = ?');
 				stmt.get([cart.idProduct, cart.idCart], (err, result) => {
 					console.log(result);
-					res.send('Success removing product');
+					res.json('Success removing product');
 				});
 			} else {
 				console.log(cart);
-				res.send('That product doesn\'t exist in the cart');
+				res.json('That product doesn\'t exist in the cart');
 			}
 		} else {
 			console.log(cart);
-			res.send('Error');
+			res.json('Error');
 		}
 	});
 });
@@ -73,15 +73,15 @@ router.get('/add/:idUser/:idProduct/:quantity', function (req, res) {
 				stmt = db.prepare('UPDATE CartItem SET quantity = ? WHERE idProduct = ? AND idCart = ?');
 				stmt.get([cart.quantity + req.params.quantity, cart.idProduct, cart.idCart], (err, result) => {
 					console.log(result);
-					res.send('Success adding another product of the same type');
+					res.json('Success adding another product of the same type');
 				});
 			} else {
 				console.log(cart);
-				res.send('That product doesn\'t exist in the cart');
+				res.json('That product doesn\'t exist in the cart');
 			}
 		} else {
 			console.log(cart);
-			res.send('Error');
+			res.json('Error');
 		}
 	});
 });
@@ -96,22 +96,22 @@ router.get('/remove/:idUser/:idProduct/:quantity', function (req, res) {
 					stmt = db.prepare('DELETE FROM CartItem WHERE idProduct = ? AND idCart = ?');
 					stmt.get([cart.idProduct, cart.idCart], (err, result) => {
 						console.log(result);
-						res.send('Success removing product');
+						res.json('Success removing product');
 					});
 				} else {
 					stmt = db.prepare('UPDATE CartItem SET quantity = ? WHERE idProduct = ? AND idCart = ?');
 					stmt.get([cart.quantity - req.params.quantity, cart.idProduct, cart.idCart], (err, result) => {
 						console.log(result);
-						res.send('Success adding another product of the same type');
+						res.json('Success adding another product of the same type');
 					});
 				}
 			} else {
 				console.log(cart);
-				res.send('That product doesn\'t exist in the cart');
+				res.json('That product doesn\'t exist in the cart');
 			}
 		} else {
 			console.log(cart);
-			res.send('Error');
+			res.json('Error');
 		}
 	});
 });
